991a2b55940138ebfc2639093cd09af6e5c30506
ttacon <ttacon@gmail.com>
2014-09-06 19:40:14 -0400

    Add base directory flag

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"os"
 )
 
+var (
+	dir = flag.String("b", "", "base directory to serve from")
+)
+
 func main() {
-	// TODO(ttacon): allow user to specify directory and just default to cwd
+	flag.Parse()
 
 	curr, err := os.Getwd()
 	if err != nil {
 		log.Fatal("os.Getwd(): ", err)
+	}
+
+	if len(*dir) > 0 {
+		if _, err := os.Stat(*dir); err != nil {
+			log.Fatal("failed to detect dir, err: ", err)
+		}
+		curr = *dir
 	}
 
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(curr))))
